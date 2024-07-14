3c9d3dcb2ce2bd366249b581030e4ee896c94d5b
Abrorbek <qodirjonov0854@gmail.com>
2024-07-14 17:00:50 +0500

    Done! exept roles and permissions

--- main/serializers.py
+++ main/serializers.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
+from .models import  *
 
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
--- main/views.py
+++ main/views.py
@@ -6,6 +6,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
+from .models import *
+from .serializers import *
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -29,4 +31,93 @@ def login_view(request):
 @api_view(['POST'])
 def logout_view(request):
     logout(request)
-    return Response({'server': 'You have successfully logged out!'}, status=status.HTTP_200_OK)
+    return Response({'success': 'You have successfully logged out!'}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def allProducts(request):
+    product = Product.objects.all()
+    serializer = ProductSerializer(product, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def createProduct(request):
+    print("Request Data:", request.data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        instance = serializer.save()
+        print("Instance Saved:", instance)
+        return Response(Product(instance).data, status=201)
+    else:
+        print("Errors:", serializer.errors)
+        return Response(serializer.errors, status=400)
+
+
+
+@api_view(['DELETE'])
+def deleteProduct(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+    product.delete()
+    return Response({'success': 'Product deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['PUT'])
+def updateProduct(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ProductSerializer(product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def allCategory(request):
+    category = Category.objects.all()
+    serializer = CategorySerializer(category, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def createCategory(request):
+    print("Request Data:", request.data)
+    serializer = CategorySerializer(data=request.data)
+    if serializer.is_valid():
+        instance = serializer.save()
+        print("Instance Saved:", instance)
+        return Response(Category(instance).data, status=201)
+    else:
+        print("Errors:", serializer.errors)
+        return Response(serializer.errors, status=400)
+
+
+@api_view(['DELETE'])
+def deleteCategory(request, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+    category.delete()
+    return Response({'success': 'Product deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['PUT'])
+def updateCategory(request, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({'error': 'Category not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = CategorySerializer(category, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
