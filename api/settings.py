73900938b87c19446948057f2d0822f32742e855
MythicalCosmic <166993229+MythicalCosmic@users.noreply.github.com>
2024-07-16 17:39:07 +0500

    Update settings.pyasd

--- api/settings.py
+++ api/settings.py
@@ -121,7 +121,10 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080/',
